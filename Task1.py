c9be552e5c97e47dea4b6fb3e0c6882106fab1cd
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-09-10 17:10:31 +0300

    Final commit

--- Task1.py
+++ Task1.py
@@ -5,3 +5,17 @@
 # ● Задача
 # Написать скрипт в любой парадигме, который выводит на экран таблицу умножения всех чисел от 1 до n.
 # Обоснуйте выбор парадигм.
+
+
+def mult_table(number):
+    for i in range(1, number+1):
+        print(f'1 * {i} = {i}')
+
+
+def main():
+    number = int(input('Enter a number: '))
+    mult_table(number)
+
+
+if __name__ == "__main__":
+    main()
