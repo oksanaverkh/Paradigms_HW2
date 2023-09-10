38847dd554eaaa78c6408cac04079ccd67b5da86
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-09-10 17:25:50 +0300

    Final commit

--- Task1.py
+++ Task1.py
@@ -6,14 +6,27 @@
 # Написать скрипт в любой парадигме, который выводит на экран таблицу умножения всех чисел от 1 до n.
 # Обоснуйте выбор парадигм.
 
+# Для решения задачи выбрана структурная и процедурная парадигма.
+# Процедурная парадигма использована вследствие необходимости использования функций проверки входящего числа и вывода результатов умножения.
+# Структурная парадигма выбрана по причине необходимости использования циклов и отсутствия целесообразности использования оператора goto.
+
+def check():
+    n = int(input('Enter a number: '))
+    while not 1 <= n <= 10:
+        print('Wrong value, try again')
+        n = int(input('Enter a number: '))
+    return int(n)
+
 
 def mult_table(number):
     for i in range(1, number+1):
-        print(f'1 * {i} = {i}')
+        print()
+        for j in range(1, number+1):
+            print(f'{i} * {j} = {i*j}')
 
 
 def main():
-    number = int(input('Enter a number: '))
+    number = check()
     mult_table(number)
 
 
